0ef5315d74f29cfb8c6d349a6a492aee079d8da8
ganesh2425 <saahoganesh023@gmail.com>
2021-03-09 16:27:54 +0530

    ok

---
+++ src/Rendering/App-PreLoc.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PrefferedLoc from ".//PrefferedLoc";
+let App =()=>{
+    return(
+        <>
+        <nav className="navbar navbar-dark bg-dark">
+            <a href="/">Preffered Locations</a>
+        </nav>
+        <PrefferedLoc/>
+        </>
+    )
+}
+export default App;
