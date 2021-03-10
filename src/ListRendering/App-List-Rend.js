3f32740c32810ac7019f083d3627830495b70f39
ganesh2425 <saahoganesh023@gmail.com>
2021-03-10 17:22:46 +0530

    ok

---
+++ src/ListRendering/App-List-Rend.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Customer from "./ListRendering/Customer";
+
+function App() {
+    return (
+        <>
+         <nav className="navbar navbar-dark bg-dark">
+             <a href="/">List Rendering</a>
+             </nav>
+             <Customer/>  
+        </>
+    )
+}
+
+export default App
